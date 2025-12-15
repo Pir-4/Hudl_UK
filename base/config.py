8fb4c05d64ec72a8f57b78a104fb04f73736f58b
Valentin Agapitov <valentin.agapitov@exness.com>
2025-12-15 13:25:34 +0000

    fix config.py

--- base/config.py
+++ base/config.py
@@ -3,6 +3,6 @@ from environs import env
 
 load_dotenv()
 
-TEST_EMAIL = env('TEST_EMAIL')
-TEST_PASSWORD = env('TEST_PASSWORD')
-LOGGER_LEVEL = env('LOGGER_LEVEL', 'INFO')
+TEST_EMAIL = env.str('TEST_EMAIL')
+TEST_PASSWORD = env.str('TEST_PASSWORD')
+LOGGER_LEVEL = env.log_level('LOGGER_LEVEL', 'INFO')
