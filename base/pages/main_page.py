1f56b6883a6fd7a44478ca7577ef7d30c238a5ec
Valentin Agapitov <valentin.agapitov@exness.com>
2025-12-15 18:00:24 +0000

    Add get_by_id method to BasePage and implement close_cookie in MainPage for cookie consent handling

--- base/pages/main_page.py
+++ base/pages/main_page.py
@@ -1,7 +1,6 @@
 from .base_page import BasePage
 
 class MainPage(BasePage):
-    ...
     def close_cookie(self):
         element = self.get_by_id('onetrust-close-btn-container')
         element.click()
