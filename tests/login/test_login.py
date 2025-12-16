6080d7f00f827e27ddb40aadff6651aaf8bd2d8f
Valentin Agapitov <valentin.agapitov@exness.com>
2025-12-16 14:54:12 +0000

    Update test_login to use static validity check for username input, ensuring consistent behavior in email validation tests.

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -18,7 +18,7 @@ def test_email_input_validation(main_page, email_validator_param):
     input_email, is_valid = email_validator_param
     steps.open_page(main_page, MAIN_PAGE_TITLE, True)
     login_page = steps.move_to_login_page(main_page)
-    steps.set_username(login_page, input_email, is_valid)
+    steps.set_username(login_page, input_email, True)
     if is_valid:
         assert_item_loaded(
             login_page.is_password_input_displayed, "Password input"
