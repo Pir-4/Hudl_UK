f9b7680df4b079ddc4b68871539f885bc0360e80
Valentin Agapitov <valentin.agapitov@exness.com>
2025-12-16 14:50:43 +0000

    Refactor test fixtures for improved clarity and update login tests to include email input validation, enhancing overall test coverage and reliability.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,12 @@ import pytest
 from base.pages import browser_factory, MainPage
 from base.config import BROWSER_NAME, TARGET_URL
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def driver(request):
     with browser_factory(BROWSER_NAME) as driver:
         yield driver
 
 
-@pytest.fixture()
+@pytest.fixture
 def main_page(driver):
     return MainPage(driver, TARGET_URL)
--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,10 +1,10 @@
 from base.config import TEST_EMAIL, TEST_PASSWORD
 from base import steps
-from base.asserts import  assert_item_loaded
+from base.asserts import  assert_item_loaded, assert_equals
 from .constants import MAIN_PAGE_TITLE
 
 
-def test_login(main_page):
+def test_happy_login_path(main_page):
     steps.open_page(main_page, MAIN_PAGE_TITLE, True)
     login_page = steps.move_to_login_page(main_page)
     steps.set_username(login_page, TEST_EMAIL, True)
@@ -12,3 +12,19 @@ def test_login(main_page):
     assert_item_loaded(
         user_main_acc_page.is_page_loaded, 'User Home Page'
     )
+
+
+def test_email_input_validation(main_page, email_validator_param):
+    input_email, is_valid = email_validator_param
+    steps.open_page(main_page, MAIN_PAGE_TITLE, True)
+    login_page = steps.move_to_login_page(main_page)
+    steps.set_username(login_page, input_email, is_valid)
+    if is_valid:
+        assert_item_loaded(
+            login_page.is_password_input_displayed, "Password input"
+        )
+    else:
+        error_message = login_page.get_error_message()
+        assert_equals(
+            'Enter a valid email.', error_message, 'Invalid email error message')
+
