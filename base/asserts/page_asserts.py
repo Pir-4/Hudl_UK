ecb477b66bc5226d74560e906e24a61dde4bbfbb
Valentin Agapitov <valentin.agapitov@exness.com>
2025-12-17 10:39:47 +0000

    Refactor assert_item_loaded function to improve parameter naming from 'page_name' to 'item_name' for better clarity in logging and assertions.

--- base/asserts/page_asserts.py
+++ base/asserts/page_asserts.py
@@ -1,5 +1,5 @@
 from base.logger import logger
 
-def assert_item_loaded(check_fuc, page_name):
-    logger.debug(f'Asserting {page_name} item loaded')
-    assert check_fuc(), f"{page_name} is not loaded"
+def assert_item_loaded(check_fuc, item_name):
+    logger.debug(f'Asserting {item_name} item loaded')
+    assert check_fuc(), f"{item_name} is not loaded"
