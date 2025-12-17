c142df907875077e8d68f87ed9982133ea094c06
Valentin Agapitov <valentin.agapitov@exness.com>
2025-12-17 10:40:20 +0000

    Fix typo in assert_item_loaded function parameter name from 'check_fuc' to 'check_func' for improved clarity and correctness in assertions.

--- base/asserts/page_asserts.py
+++ base/asserts/page_asserts.py
@@ -1,5 +1,5 @@
 from base.logger import logger
 
-def assert_item_loaded(check_fuc, item_name):
+def assert_item_loaded(check_func, item_name):
     logger.debug(f'Asserting {item_name} item loaded')
-    assert check_fuc(), f"{item_name} is not loaded"
+    assert check_func(), f"{item_name} is not loaded"
